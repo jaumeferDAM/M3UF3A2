a6bd00c538785f21b764835e14c962e8707e000b
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2016-11-17 13:17:21 +0100

    casi

--- src/domino/Domino.java
+++ src/domino/Domino.java
@@ -1,20 +1,36 @@
 package domino;
 
+import domino.control.GestioDomino;
+import domino.model.Joc;
+import domino.model.Jugador;
 import domino.vista.InterficieGrafica;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.swing.SwingUtilities;
 
 public class Domino {
 
- public static void main(String[] args) {
-                            SwingUtilities.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                           
-                            InterficieGrafica intGraf = new InterficieGrafica();
-                            intGraf.setVisible(true);
-                        }
-                    });
-        
+    public static void main(String[] args) {
+        Joc joc = new Joc(4, 28, 7);
+
+        InterficieGrafica ig = new InterficieGrafica();
+        GestioDomino gd = new GestioDomino(ig);
+        String[] noms = {"Pau", "fd", "dd", "dd2"};
+        joc.iniciar(noms);
+        System.out.println((joc.getJugadors()[0]));
+
+        gd.iniicia();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                InterficieGrafica intGraf = new InterficieGrafica();
+                intGraf.setVisible(true);
+            }
+        });
+
     }
 
 }
